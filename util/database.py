9aa32b3b427f4e3066c8986a0a7a5bf2410218ed
Alejandro Garcia <re.alejandro.garcia@gmail.com>
2022-03-10 15:27:01 -0600

    Removed database.py

--- util/database.py
+++
@@ -1,4 +0,0 @@
-from env import username, password, hostname
-
-def get_db_url(database_name, username = username, password = password, hostname = hostname):
-    return f'mysql+pymysql://{username}:{password}@{hostname}/{database_name}'
